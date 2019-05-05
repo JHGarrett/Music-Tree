92ef878b6c24a8bcb33bc3aa067e08d59128de59
John Garrett <infantrygarrett@icloud.com>
2019-05-04 22:36:36 -0500

    added store for redux using thunk

--- client/src/helpers/store.js
+++ client/src/helpers/store.js
@@ -1,11 +1,19 @@
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
-import { createLogger } from "redux-logger"; // must be LAST middleware in chain according to docs
 import RootReducer from "../reducers";
+import { createLogger } from "redux-logger"; // must be LAST middleware in chain according to docs
 
 const loggerMiddleware = createLogger();
 
 
-// 
+// create store for redux
+
+
+// apply redux thunk- will return functions instead of actions
+
+
+// apply redux logger-- will show errors in the log
 
 export const store = createStore(RootReducer, applyMiddleware(thunkMiddleware));
+
+loggerMiddleware
