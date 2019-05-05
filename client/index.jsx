fbfb6ef0ffbfb6dcd5dd2ad136d45cc425c6c378
John Garrett <infantrygarrett@icloud.com>
2019-05-04 22:23:11 -0500

    removed mo

---
+++ client/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import AppRoutes from "./src/routing/AppRoutes.jsx";
+import { store } from "./src/helpers/store";
+import styles from "./src/styles/main.scss";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <AppRoutes />
+  </Provider>,
+  document.getElementById("root")
+);
+
+// use in development for hot module reloading
+if (module.hot) {
+  module.hot.accept();
+}
