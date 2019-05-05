a88d90d78a22293767675e528a49fb032d0e20fb
John Garrett <infantrygarrett@icloud.com>
2019-05-05 13:05:52 -0500

    working on the home page

---
+++ client/src/components/pages/landing/Home.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import RectDOM from 'react-dom';
+import { Link, withRouter } from 'react-router-dom';
+
+class Home extends Component {
+  handleSearchInput(e) {
+    e.preventDefault();
+    const search = e.targe.elements.search.value;
+    this.props.history.push(`/search/${search}`);
+  }
+
+render(){
+  return(
+    <div className={this.props.launch ? "page-wrapper nodash" : "page-wrapper"}>
+      <div className="landing-page-title">
+      <h1>Discover new music!</h1>
+      </div>
+      <div className="searchbar-wrapper">
+      <h2>Type in a song or Artist</h2>
+      <form className="landing-searchbar" onSubmit={}
+      </div>
+    </div>
+  )
+}
+
+}
