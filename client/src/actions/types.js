bb4ede3e4d1350753dcd1ca9e24fb9310c963252
John Garrett <infantrygarrett@icloud.com>
2019-05-04 22:20:35 -0500

    removed files

--- client/src/actions/types.js
+++
@@ -1,5 +0,0 @@
-export const FETCH_USER = 'FETCH_USER'; //change to AUTH_USER? how is fetch different from loginuser?
-export const UPDATE_USER = 'UPDATE_USER';
-export const DELETE_USER = 'DELETE_USER';
-export const LOGOUT_USER = 'LOGOUT_USER';
-export const AUTH_ERROR =  'AUTH_USER';
