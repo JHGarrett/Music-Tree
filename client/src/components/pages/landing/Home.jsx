638ad1819c0bbaf20f84a0732dfe99a79167e01e
John Garrett <infantrygarrett@icloud.com>
2019-05-05 20:00:51 -0500

    formatted with prettier

--- client/src/components/pages/landing/Home.jsx
+++ client/src/components/pages/landing/Home.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import RectDOM from 'react-dom';
-import { Link, withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import RectDOM from "react-dom";
+import { Link, withRouter } from "react-router-dom";
 
 class Home extends Component {
   handleSearchInput(e) {
@@ -9,24 +9,30 @@ class Home extends Component {
     this.props.history.push(`/search/${search}`);
   }
 
-render(){
-  console.log('from Home', this.props);
-  return(
-    <div className={this.props.launch ? "page-wrapper nodash" : "page-wrapper"}>
-      <div className="landing-page-title">
-      <h1>Discover new music!</h1>
+  render() {
+    console.log("from Home", this.props);
+    return (
+      <div
+        className={this.props.launch ? "page-wrapper nodash" : "page-wrapper"}
+      >
+        <div className="landing-page-title">
+          <h1>Discover new music!</h1>
+        </div>
+        <div className="searchbar-wrapper">
+          <h2>Type in a song or Artist</h2>
+          <form
+            className="landing-searchbar"
+            onSubmit={this.handleSearchInput.bind(this)}
+          >
+            <button type="submit" className="landing-search-icon">
+              <i className="fas fa-seach" />
+            </button>
+            <input className="landing-search" name="search" />
+          </form>
+        </div>
       </div>
-      <div className="searchbar-wrapper">
-      <h2>Type in a song or Artist</h2>
-      <form className="landing-searchbar" onSubmit={this.handleSearchInput.bind(this)}>
-            <button type="submit" className="landing-search-icon"><i className="fas fa-seach"></i></button>
-            <input className="landing-search" name="search"/>
-              </form>
-      </div>
-    </div>
-  )
-}
-
+    );
+  }
 }
 
 export default withRouter(Home);
--- client/src/components/pages/landing/PlayVideo.jsx
+++ client/src/components/pages/landing/PlayVideo.jsx
@@ -1,50 +1,63 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { S_IFLNK } from 'constants';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { S_IFLNK } from "constants";
 
-const PlayVideo = (props) => {
-  console.log('playvideo props: ', props);
-  window.scrollTo(0, 0)
+const PlayVideo = props => {
+  console.log("playvideo props: ", props);
+  window.scrollTo(0, 0);
 
   if (props.location) {
     const { title, description, url } = props.location.state.video;
 
     return (
       <div className="page-wrapper">
-      <iframe src={`https://www.youtube.com/embed/${url}`} className="main-video"></iframe>
-      <h3>{title}</h3>
-      <p>{description}</p>
+        <iframe
+          src={`https://www.youtube.com/embed/${url}`}
+          className="main-video"
+        />
+        <h3>{title}</h3>
+        <p>{description}</p>
       </div>
     );
   } else if (props.selectedVideo) {
     const { title, description, thumbnail, url } = props.selectedVideo;
     const yturl = `https://www.youtube.com/embed/${url}`;
 
-    return(
+    return (
       <div>
         <div>
-          <iframe src={yturl} className="main-video"></iframe>
+          <iframe src={yturl} className="main-video" />
         </div>
         <div>
           <div>{title}</div>
           <div>{description}</div>
-          {props.auth.loggedIn && 
+          {props.auth.loggedIn && (
             <div>
-              <button className="save-video-button" onClick={() => props.dispatch(addLikedVideo(props.auth.creds._id, props.selectedVideo))}><i className="fas fa-heart"></i></button>
-              <button className="save-video-button"><i className="fas fa-list"></i><i className="fas fa-plus"></i></button>  
-            </div>  
-            
-            }
+              <button
+                className="save-video-button"
+                onClick={() =>
+                  props.dispatch(
+                    addLikedVideo(props.auth.creds._id, props.selectedVideo)
+                  )
+                }
+              >
+                <i className="fas fa-heart" />
+              </button>
+              <button className="save-video-button">
+                <i className="fas fa-list" />
+                <i className="fas fa-plus" />
+              </button>
+            </div>
+          )}
         </div>
       </div>
     );
-
   } else {
     return null;
   }
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     auth: state.auth
   };
