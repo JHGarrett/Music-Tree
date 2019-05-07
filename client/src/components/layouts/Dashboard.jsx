838c0e078a847c2b09d0f3825960bb09423bd73f
John Garrett <infantrygarrett@icloud.com>
2019-05-06 22:20:40 -0500

    ran prettier

--- client/src/components/layouts/Dashboard.jsx
+++ client/src/components/layouts/Dashboard.jsx
@@ -1,37 +1,42 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import { connenct } from 'react-redux';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import { connenct } from "react-redux";
 
-import DashTop from './DashbordCompnents/Dashtop.jsx';
-import DashTopUser from './DashboardComponents/DashTopUser.jsx';
-import DashBottom from './DashboardComponents/DashBottom.jsx';
+import DashTop from "./DashbordCompnents/Dashtop.jsx";
+import DashTopUser from "./DashboardComponents/DashTopUser.jsx";
+import DashBottom from "./DashboardComponents/DashBottom.jsx";
 
 class DashBoard extends Component {
   componentDidMount() {
     // get json data
-    let session =JSON.parse(sessionStorage.getItem('session'));
+    let session = JSON.parse(sessionStorage.getItem("session"));
     this.props.auth.loggedIn ? this.props.handleShowDash() : null;
   }
 
   render() {
-
-    console.log('from dashboard: ', this.props);
+    console.log("from dashboard: ", this.props);
     const { loggedIn, creds } = this.props.auth;
 
     return (
-      <div className={this.UNSAFE_componentWillMount.props.launch ? "hide-dash" : "dashboard"}>
-        {loggedIn ?
-          <DashTopUser
-            retrieveSavedVideos={this.props.retrieveSavedVideos}
-            /> :
-            <dashTop />}
-          <DashBottom />
+      <div
+        className={
+          this.UNSAFE_componentWillMount.props.launch
+            ? "hide-dash"
+            : "dashboard"
+        }
+      >
+        {loggedIn ? (
+          <DashTopUser retrieveSavedVideos={this.props.retrieveSavedVideos} />
+        ) : (
+          <DashTop />
+        )}
+        <DashBottom />
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     auth: state.auth
   };
