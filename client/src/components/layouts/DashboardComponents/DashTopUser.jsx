4848a81bf9731a758632a98dfcae97db4abdae9b
John Garrett <infantrygarrett@icloud.com>
2019-05-06 20:34:48 -0500

    added user dash

---
+++ client/src/components/layouts/DashboardComponents/DashTopUser.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+export default class DashBottom extends Component {
+  render() {
+    return (
+      <div className="dashboard-bottom">
+        <div className="dashboard-social-media">
+          <a className="social-media" href="#">
+            <i className="fab fa-facebook-f" />
+          </a>
+          <a className="social-media" href="#">
+            <i className="fab fa-instagram" />
+          </a>
+          <a className="social-media" href="#">
+            <i className="fab fa-twitter" />
+          </a>
+          <a className="social-media" href="#">
+            <i className="fab fa-github" />
+          </a>
+        </div>
+
+        <div className="dashboard-footer">
+          <p>
+            Copyright &copy; 2019<span> </span>
+            <a
+              className="dashboard__link"
+              href="https://github.com/JHGarrett/Music-Tree"
+            >
+              John Garrett
+            </a>
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
