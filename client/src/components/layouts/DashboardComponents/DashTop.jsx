4c9d83cf1c8ebebe8c772d64ae2ec108e765239f
John Garrett <infantrygarrett@icloud.com>
2019-05-06 22:13:38 -0500

    added signup dash

---
+++ client/src/components/layouts/DashboardComponents/DashTop.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+export default class DashTop extends Component {
+  render() {
+    return (
+      <div className="dashboard-top">
+        <h2 className="dash-header">Having Fun?</h2>
+        <p className="sign-up">
+          <NavLink className="dashboard__link" to="/signup">
+            Sign up
+          </NavLink>{" "}
+          These are some of the benefits:
+        </p>
+        <ul className="benefits-list">
+          <li>- Profile Picture</li>
+          <li>- Custom themes</li>
+          <li>- Save your liked videos</li>
+          <p className="bold">And much more!</p>
+        </ul>
+      </div>
+    );
+  }
+}
