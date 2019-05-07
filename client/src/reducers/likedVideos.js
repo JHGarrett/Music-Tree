e64ed9cf9b5d0839a6b9268d629fc3747cd29255
John Garrett <infantrygarrett@icloud.com>
2019-05-07 15:55:42 -0500

    added liked video func

--- client/src/reducers/likedVideos.js
+++ client/src/reducers/likedVideos.js
@@ -6,6 +6,30 @@ import {
   DELETE_USER_VIDEO
 } from "../actions/userVideos";
 
-let videos = JSON.parse(sessionStorage.getItem('videos'));
+let videos = JSON.parse(sessionStorage.getItem("videos"));
 const initialState = videos ? { videos } : {};
 
+export default (state = initialState, action) => {
+  switch (action.type) {
+    // adding a liked video to user
+    case ADD_LIKED_VIDEO:
+      return {
+        likedVideos: action.payload
+      };
+
+    case FETCH_USER_VIDEO:
+      // getting a liked video from the user db
+      return {
+        likedVideos: action.payload
+      };
+
+    case DELETE_USER_VIDEO:
+      // deleting a user liked video... BYe
+      return {
+        likedVideos: action.payload
+      };
+
+    default:
+      return state;
+  }
+};
