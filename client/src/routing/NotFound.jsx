5aa537af6835f67a09f52e59b82a87bcf428c6df
John Garrett <infantrygarrett@icloud.com>
2019-05-07 22:04:39 -0500

    fixed issue with polyfill

---
+++ client/src/routing/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFound = () => (
+  <div className="not-found page-wrapper nodash">
+    <h2>
+      <span>404 Error!</span> Page not found.
+    </h2>
+    <Link to="/">Go to Homepage</Link>
+  </div>
+);
+
+export default NotFound;
