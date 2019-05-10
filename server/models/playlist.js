b44d04e76826c738827b20c6b88f5d3365716fb7
John Garrett <infantrygarrett@icloud.com>
2019-05-09 22:49:17 -0500

    routes for playlist

---
+++ server/models/playlist.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const playlistSchema = new mongoose.Schema({
+  name: String,
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    username: String
+  },
+  videos: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video"
+    }
+  ]
+});
+
+//////// Virtual Types
+
+playlistSchema.virtual("videosCount").get(function() {
+  //keep the function, don't refactor to an arrow function here
+  return this.videos.lenght;
+});
+
+module.exports = mongoose.model("Playlist", playlistSchema);
